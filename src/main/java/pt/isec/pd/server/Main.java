e3428c4f75b4081bd9978cf8c010023e64b85b09
Dario Santosd <a2021110772@isec.pt>
2025-10-28 14:14:46 +0000

    add SpringBoot

--- src/main/java/pt/isec/pd/server/Main.java
+++ src/main/java/pt/isec/pd/server/Main.java
@@ -1,7 +1,12 @@
 package pt.isec.pd.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "pt.isec.pd")
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello server!");
+        SpringApplication.run(Main.class, args);
     }
 }
